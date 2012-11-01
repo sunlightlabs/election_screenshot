f50a27db86f4c41880a68de95a1b6e90672e4773
Kaitlin Lee <katycorp@gmail.com>
2012-11-01 14:49:38 -0400

    help text

--- election_screenshot/state_urls/models.py
+++ election_screenshot/state_urls/models.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 class ElectionUrl(models.Model):
     index_url = models.URLField(help_text="URL of the index page for the board of elections")
     url = models.URLField(help_text="url to be screenshotted")
-    state = models.CharField(max_length=2)
+    state = models.CharField(max_length=2, help_text="Two char abbreviation for the state, AL e.g.")
 
 
 class ElectionScreenshot(models.Model):
